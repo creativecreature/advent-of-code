0a957910af096a3852e634a348d607e35b7dead6
Victor Conner <hello@creativecreature.com>
2022-12-16 20:52:06 +0100

    WIP

---
+++ 2022/day16/day16.go
@@ -0,0 +1,180 @@
+package day16
+
+import (
+	"bufio"
+	"io"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type node struct {
+	name  string
+	rate  int
+	nodes []string
+}
+
+type workitem struct {
+	node  *node
+	count int
+}
+
+var pattern = regexp.MustCompile("Valve (?P<name>[A-Z].) has flow rate=(?P<rate>[0-9]+); tunnels? leads? to valves? (?P<nodes>.*)")
+
+func parseInput(input io.Reader) map[string]*node {
+	scanner := bufio.NewScanner(input)
+	nodeMap := make(map[string]*node)
+	for scanner.Scan() {
+		match := pattern.FindStringSubmatch(scanner.Text())
+		name := match[pattern.SubexpIndex("name")]
+		rate, _ := strconv.Atoi(match[pattern.SubexpIndex("rate")])
+		nodes := strings.Split(match[pattern.SubexpIndex("nodes")], ", ")
+		node := &node{name, rate, nodes}
+		nodeMap[name] = node
+	}
+	return nodeMap
+}
+
+func generateHeatmap(nodes map[string]*node) map[string]map[string]int {
+	heatmap := map[string]map[string]int{}
+	for _, n := range nodes {
+		nodeMap := make(map[string]int)
+		queue := []workitem{{n, 0}}
+
+		if n.rate == 0 && n.name != "AA" {
+			continue
+		}
+
+		for len(queue) > 0 {
+			currentItem := queue[0]
+			queue = queue[1:]
+
+			if _, ok := nodeMap[currentItem.node.name]; ok {
+				continue
+			}
+
+			nodeMap[currentItem.node.name] = currentItem.count
+
+			for _, c := range currentItem.node.nodes {
+				child := nodes[c]
+				queue = append(queue, workitem{node: child, count: currentItem.count + 1})
+			}
+		}
+
+		// We don't need to visit any zeros
+		for n := range nodeMap {
+			node := nodes[n]
+			if node.rate == 0 {
+				delete(nodeMap, n)
+			}
+		}
+		heatmap[n.name] = nodeMap
+	}
+	return heatmap
+}
+
+func rec(currentNode string, nodes map[string]*node, heatmap map[string]map[string]int, visisted map[string]bool, steps int) int {
+	if _, ok := visisted[currentNode]; ok {
+		return 0
+	}
+
+	if steps <= 1 {
+		return 0
+	}
+
+	visisted[currentNode] = true
+	m := heatmap[currentNode]
+	bestChild := 0
+	for n := range m {
+		i := m[n]
+		newMap := make(map[string]bool)
+		for k, v := range visisted {
+			newMap[k] = v
+		}
+		heatmapScore := i + 2
+		childCount := float64(rec(n, nodes, heatmap, newMap, steps-(heatmapScore-1)))
+		bestChild = int(math.Max(float64(bestChild), childCount))
+	}
+
+	return bestChild + nodes[currentNode].rate*steps
+}
+
+func recTwo(nodeOne, nodeTwo string, nodes map[string]*node, heatmap map[string]map[string]int, visisted map[string]bool, stepsOne, stepsTwo int) int {
+	if _, ok := visisted[nodeOne]; ok {
+		return 0
+	}
+
+	if _, ok := visisted[nodeTwo]; ok {
+		return 0
+	}
+
+	if stepsOne <= 1 && stepsTwo <= 1 {
+		return 0
+	}
+
+	if stepsOne > 1 {
+		visisted[nodeOne] = true
+	}
+
+	if stepsTwo > 1 {
+		visisted[nodeTwo] = true
+	}
+	nodeOneHeatMap := heatmap[nodeOne]
+	nodeTwoHeatMap := heatmap[nodeTwo]
+
+	bestChild := 0
+	for n := range nodeOneHeatMap {
+		for m := range nodeTwoHeatMap {
+			if m == n {
+				continue
+			}
+
+			nodeOneHeatmapScore := nodeOneHeatMap[n]
+			nodeTwoHeatmapScore := nodeOneHeatMap[m]
+
+			newMap := make(map[string]bool)
+			for k, v := range visisted {
+				newMap[k] = v
+			}
+
+			heatmapScoreNodeOne := nodeOneHeatmapScore + 2
+			heatmapScoreNodeTwo := nodeTwoHeatmapScore + 2
+
+			childCount := float64(recTwo(n, m, nodes, heatmap, newMap, stepsOne-(heatmapScoreNodeOne-1), stepsTwo-(heatmapScoreNodeTwo-1)))
+			bestChild = int(math.Max(float64(bestChild), childCount))
+		}
+	}
+
+	total := 0
+	resultOne := (nodes[nodeOne].rate) * stepsOne
+	resultTwo := (nodes[nodeTwo].rate) * stepsTwo
+
+	if stepsOne > 1 {
+		total = total + resultOne
+	}
+
+	if stepsTwo > 1 {
+		total = total + resultTwo
+	}
+	return bestChild + total
+}
+
+func PartOne(input io.Reader) int {
+	nodes := parseInput(input)
+	heatmap := generateHeatmap(nodes)
+
+	wuu := rec("AA", nodes, heatmap, map[string]bool{}, 30)
+
+	// fmt.Println(order)
+	return wuu
+}
+
+func PartTwo(input io.Reader) int {
+	nodes := parseInput(input)
+	heatmap := generateHeatmap(nodes)
+	wuu := recTwo("AA", "AA", nodes, heatmap, map[string]bool{}, 26, 26)
+
+	// fmt.Println(order)
+	return wuu
+}
---
+++ 2022/day16/day16_test.go
@@ -0,0 +1,47 @@
+package day16_test
+
+import (
+	"os"
+	"testing"
+
+	"day16"
+)
+
+func TestPartOneExampleInput(t *testing.T) {
+	input, err := os.Open("./testdata/example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := 1651
+	got := day16.PartOne(input)
+	if want != got {
+		t.Errorf("wanted %d; got %d", want, got)
+	}
+}
+
+func TestPartOneRealInput(t *testing.T) {
+	input, err := os.Open("./testdata/input.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := 1754
+	got := day16.PartOne(input)
+	if want != got {
+		t.Errorf("wanted %d; got %d", want, got)
+	}
+}
+
+func TestPartTwoExampleInput(t *testing.T) {
+	input, err := os.Open("./testdata/example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := 1707
+	got := day16.PartTwo(input)
+	if want != got {
+		t.Errorf("wanted %d; got %d", want, got)
+	}
+}
