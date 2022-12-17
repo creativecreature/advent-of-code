ae0f3e98401e2de6cbff99b1d26b70f4fae4c76c
Victor Conner <hello@creativecreature.com>
2022-12-17 17:12:10 +0100

    WIP

--- 2022/day17/day17.go
+++ 2022/day17/day17.go
@@ -3,6 +3,8 @@ package day17
 import (
 	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
 
 func getBlock(blockType, y int) [][]int {
@@ -82,6 +84,31 @@ func createKey(x, y int) string {
 	return fmt.Sprintf("%d-%d", x, y)
 }
 
+func checkCollision(seen map[string]bool, block [][]int) bool {
+	for _, pos := range block {
+		key := createKey(pos[0], pos[1])
+		if seen[key] {
+			return true
+		}
+	}
+	return false
+}
+
+func getNewTop(seen map[string]bool) int {
+	max := 0
+	for key := range seen {
+		pos := strings.Split(key, "-")
+		y, err := strconv.Atoi(pos[1])
+		if err != nil {
+			panic(err)
+		}
+		if y > max {
+			max = y
+		}
+	}
+	return max
+}
+
 func PartOne(input io.Reader) int {
 	// Push the floor into the "seen" map
 	seen := make(map[string]bool)
@@ -90,8 +117,36 @@ func PartOne(input io.Reader) int {
 		seen[key] = true
 	}
 
-	for t := 1; t <= 2022; t++ {
-		block := getBlock(t%5, 1)
+	windIndex := 0
+	currentTop := 0
+	for t := currentTop; t < 2022; t++ {
+		block := getBlock(t%5, currentTop+4)
+		for {
+			w := parsedInput[windIndex]
+			if w == '<' {
+				moveLeft(block)
+				if checkCollision(seen, block) {
+					moveRight(block)
+				}
+			} else {
+				moveRight(block)
+				if checkCollision(seen, block) {
+					moveLeft(block)
+				}
+			}
+			windIndex = (windIndex + 1) % len(parsedInput)
+			moveDown(block)
+			if checkCollision(seen, block) {
+				moveUp(block)
+				break
+			}
+		}
+		for _, pos := range block {
+			key := createKey(pos[0], pos[1])
+			seen[key] = true
+		}
+		currentTop = getNewTop(seen)
 	}
-	return -1
+
+	return getNewTop(seen)
 }
